e67c00f2bcc8cd152961f172cff01f8accc52eed
iceStorm <hauduedaiduong@gmail.com>
2022-03-18 01:11:39 +0700

    refactor oauth buttons to render by .map

--- src/pages/login/oauth-login/index.tsx
+++ src/pages/login/oauth-login/index.tsx
@@ -3,129 +3,71 @@ import Image from "next/image"
 import styles from "./OAuth-button.module.css"
 
 export default function OAuthLoginView() {
+    const OAUTH_PROVIDERS = [
+        {
+            name: "Github",
+            authUrl: `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_OAUTH_CLIENT_ID}`,
+            iconUrl: "/oauth/github.svg",
+        },
+        {
+            name: "Google",
+            authUrl: "#",
+            iconUrl: "/oauth/google.svg",
+        },
+        {
+            name: "Twitter",
+            authUrl: "#",
+            iconUrl: "/oauth/twitter.svg",
+        },
+        {
+            name: "Facebook",
+            authUrl: "#",
+            iconUrl: "/oauth/facebook.svg",
+        },
+        {
+            name: "Instagram",
+            authUrl: "#",
+            iconUrl: "/oauth/instagram.svg",
+        },
+        {
+            name: "Microsoft",
+            authUrl: "#",
+            iconUrl: "/oauth/microsoft.svg",
+        },
+        {
+            name: "Linkedin",
+            authUrl: "#",
+            iconUrl: "/oauth/linkedin.svg",
+        },
+        {
+            name: "Tiktok",
+            authUrl: "#",
+            iconUrl: "/oauth/tiktok.svg",
+        },
+    ]
+
     return (
         <div className={clsx(styles.oauthGrid)}>
-            {/* Github */}
-            <a
-                className={clsx(
-                    styles.oauthButton,
-                    "bg-gray-100 hover:bg-gray-200 dark:hover:bg-slate-900 dark:border-slate-700 dark:bg-slate-800 "
-                )}
-                href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_OAUTH_CLIENT_ID}`}
-            >
-                <Image src="/oauth/github.svg" alt="" width="30" height="30" />
-                <span>Github</span>
-            </a>
-
-            {/* Google */}
-            <a
-                href="#"
-                className={clsx(
-                    styles.oauthButton,
-                    "bg-gray-100 hover:bg-gray-200 dark:hover:bg-slate-900 dark:border-slate-700 dark:bg-slate-800 "
-                )}
-            >
-                <Image src="/oauth/google.svg" alt="" width="30" height="30" />
-                <span>Google</span>
-            </a>
-
-            {/* Twitter */}
-            <a
-                href="#"
-                className={clsx(
-                    styles.oauthButton,
-                    "bg-gray-100 hover:bg-gray-200 dark:hover:bg-slate-900 dark:border-slate-700 dark:bg-slate-800 "
-                )}
-            >
-                <Image src="/oauth/twitter.svg" alt="" width="30" height="30" />
-                <span>Twitter</span>
-            </a>
-
-            {/* Facebook */}
-            <a
-                href="#"
-                className={clsx(
-                    styles.oauthButton,
-                    "bg-gray-100 hover:bg-gray-200 dark:hover:bg-slate-900 dark:border-slate-700 dark:bg-slate-800 "
-                )}
-            >
-                <Image
-                    src="/oauth/facebook.svg"
-                    alt=""
-                    width="30"
-                    height="30"
-                />
-                <span>Facebook</span>
-            </a>
-
-            {/* Instagram */}
-            <a
-                href="#"
-                className={clsx(
-                    styles.oauthButton,
-                    "bg-gray-100 hover:bg-gray-200 dark:hover:bg-slate-900 dark:border-slate-700 dark:bg-slate-800 "
-                )}
-            >
-                <Image
-                    src="/oauth/instagram.svg"
-                    alt=""
-                    width="30"
-                    height="30"
-                />
-                <span>Instagram</span>
-            </a>
-
-            {/* Microsoft */}
-            <a
-                href="#"
-                className={clsx(
-                    styles.oauthButton,
-                    "bg-gray-100 hover:bg-gray-200 dark:hover:bg-slate-900 dark:border-slate-700 dark:bg-slate-800 "
-                )}
-            >
-                <Image
-                    src="/oauth/microsoft.svg"
-                    alt=""
-                    width="30"
-                    height="30"
-                />
-                <span>Microsoft</span>
-            </a>
-
-            {/* Linkedin */}
-            <a
-                href="#"
-                className={clsx(
-                    styles.oauthButton,
-                    "bg-gray-100 hover:bg-gray-200 dark:hover:bg-slate-900 dark:border-slate-700 dark:bg-slate-800 "
-                )}
-            >
-                <Image
-                    src="/oauth/linkedin.svg"
-                    alt=""
-                    width="30"
-                    height="30"
-                />
-                <span>Linkedin</span>
-            </a>
-
-
-            {/* Tiktok */}
-            <a
-                href="#"
-                className={clsx(
-                    styles.oauthButton,
-                    "bg-gray-100 hover:bg-gray-200 dark:hover:bg-slate-900 dark:border-slate-700 dark:bg-slate-800 "
-                )}
-            >
-                <Image
-                    src="/oauth/tiktok.svg"
-                    alt=""
-                    width="30"
-                    height="30"
-                />
-                <span>Tiktok</span>
-            </a>
+            {OAUTH_PROVIDERS.map((provider, index) => {
+                return (
+                    <a
+                        key={index}
+                        className={clsx(
+                            styles.oauthButton,
+                            "bg-gray-100 hover:bg-gray-200 dark:hover:bg-slate-900 dark:border-slate-700 dark:bg-slate-800 "
+                        )}
+                        href={provider.authUrl}
+                    >
+                        <Image
+                            src={provider.iconUrl}
+                            alt=""
+                            width="30"
+                            height="30"
+                        />
+                        <span>{provider.name}</span>
+                    </a>
+                )
+            })}
         </div>
     )
 }
